44ff00f6798964e9a75fb6776b7ca707a53c7c4c
TefaSMKWS24 <tefaproject.smkws@gmail.com>
2025-01-23 09:30:58 +0700

    modified:   database/migrations/2025_01_18_044003_create_barangs_table.php

--- database/migrations/2025_01_18_044003_create_barangs_table.php
+++ database/migrations/2025_01_18_044003_create_barangs_table.php
@@ -14,8 +14,8 @@ return new class extends Migration
         Schema::create('barang', function (Blueprint $table) {
             $table->string('kode_barang',6)->unique()->primary();
             $table->string('nama_barang',30);
-            $table->bigInteger('harga');
-            $table->bigInteger('stok');
+            $table->bigInteger('harga',6);
+            $table->bigInteger('stok',3);
             $table->timestamps();
         });
     }
