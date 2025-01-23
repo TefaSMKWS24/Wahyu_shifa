96c6d77228409edafcd8b5973b176fa7bf9098c9
TefaSMKWS24 <tefaproject.smkws@gmail.com>
2025-01-23 08:32:10 +0700

    new file:   app/Models/transaksi.php 	new file:   database/migrations/2025_01_23_011724_create_transaksis_table.php

---
+++ database/migrations/2025_01_23_011724_create_transaksis_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transaksis', function (Blueprint $table) {
+            $table->string('kode_transaksi',6)->unique()->primary();
+            $table->date('tanggal_transaksi');
+            $table->string('kode_pelanggan',6);
+            $table->bigInteger('total_belanja');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transaksis');
+    }
+};
