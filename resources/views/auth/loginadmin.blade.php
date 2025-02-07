79acbc2fb0bc63d2d2bec13a1eca867750d4aab7
TefaSMKWS24 <tefaproject.smkws@gmail.com>
2025-02-07 08:12:08 -0800

    modified:   resources/views/auth/loginadmin.blade.php 	modified:   resources/views/auth/loginkasir.blade.php

--- resources/views/auth/loginadmin.blade.php
+++ resources/views/auth/loginadmin.blade.php
@@ -57,6 +57,7 @@
         <div class="card-body login-card-body">
           <p class="login-box-msg">Sign in to start your session</p>
           <form action="../index3.html" method="post">
+            @csrf
             <div class="input-group mb-3">
               <input type="email" name="email" id="email" class="form-control" placeholder="Email" />
               <div class="input-group-text"><span class="bi bi-envelope"></span></div>
